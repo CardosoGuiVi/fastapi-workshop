822fd355eb54ad91d486c77cdd906db0a630a822
Guilherme Cardoso <cardoso.guivi@gmail.com>
2024-05-14 23:24:48 -0300

    FEAT: Add a lot of things :)

--- newx/app.py
+++ newx/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .routes import main_router
+
 
 app = FastAPI(
     title="NewX",
     version="0.1.0",
     description="NewX is a posting app",
 )
+
+app.include_router(main_router)
--- newx/models/user.py
+++ newx/models/user.py
@@ -1,8 +1,11 @@
 """User related data models"""
 from typing import Optional
 from sqlmodel import Field, SQLModel
+from newx.security import HashedPassword
+from pydantic import BaseModel
 
 
+# Models
 class User(SQLModel, table=True):
     """Represents the User Model"""
 
@@ -11,4 +14,23 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, nullable=False)
     avatar: Optional[str] = None
     bio: Optional[str] = None
-    password: str = Field(nullable=False)
+    password: HashedPassword
+
+
+# Serializers
+class UserResponse(BaseModel):
+    """Serializer for User Response"""
+
+    username: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+
+class UserRequest(BaseModel):
+    """Serializer for User request payload"""
+
+    email: str
+    username: str
+    password: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
